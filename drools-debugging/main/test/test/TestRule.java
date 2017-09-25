37efbb04bb117dca604f8de32990dbefe0d61379
eggsandbutter <rudyatleti@gmail.com>
2017-09-25 17:39:09 +0200

    added another nested accessor, pojo

--- drools-debugging/main/test/test/TestRule.java
+++ drools-debugging/main/test/test/TestRule.java
@@ -32,53 +32,40 @@ public class TestRule {
 			kieSession = kieContainer.newKieSession("ksession-rules");
 	        kieSession.addEventListener(new RuleRuntimeEventListener() {
 	 
-	        	@Override
-	            public void objectInserted(ObjectInsertedEvent objectInsertedEvent) {
+	        	public void objectInserted(ObjectInsertedEvent objectInsertedEvent) {
 					System.out.println("INSERT: "+ objectInsertedEvent.getObject().toString());				
 	            }
-				@Override
 	            public void objectUpdated(ObjectUpdatedEvent objectUpdatedEvent) {
 					System.out.println("UPDATE: "+ objectUpdatedEvent.getObject().toString());				
 	            }
-				@Override
 	            public void objectDeleted(ObjectDeletedEvent objectDeletedEvent) {
 					System.out.println("DELETE: "+ objectDeletedEvent.getOldObject().toString());				
 	            }
 	        });
 	        kieSession.addEventListener(new AgendaEventListener() {
 				
-				@Override
 	            public void matchCreated(MatchCreatedEvent event) {
 					System.out.println(event.getMatch().getRule().getName()+ " can be fired in agenda");				
 	            }
-				@Override
 	            public void matchCancelled(MatchCancelledEvent event) {
 					System.out.println(event.getMatch().getRule().getName()+ " cannot be in agenda");				
 	            }
-				@Override
 				public void beforeMatchFired(BeforeMatchFiredEvent event) {
 					System.out.println(event.getMatch().getRule().getName()+ " will be fired");				
 	            }
-				@Override
 	            public void afterMatchFired(AfterMatchFiredEvent event) {
 					System.out.println(event.getMatch().getRule().getName()+ " has been fired");				
 	            }
-				@Override
 	            public void agendaGroupPopped(AgendaGroupPoppedEvent event) {
 	            }
-				@Override
 	            public void agendaGroupPushed(AgendaGroupPushedEvent event) {
 	            }
-				@Override
 	            public void beforeRuleFlowGroupActivated(RuleFlowGroupActivatedEvent event) {
 	            }
-				@Override
 	            public void afterRuleFlowGroupActivated(RuleFlowGroupActivatedEvent event) {
 	            }
-				@Override
 	            public void beforeRuleFlowGroupDeactivated(RuleFlowGroupDeactivatedEvent event) {
 	            }
-				@Override
 	            public void afterRuleFlowGroupDeactivated(RuleFlowGroupDeactivatedEvent event) {
 	            }
 	        });
